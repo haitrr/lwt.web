17adee0970d7dffbc6c4f2757ab8dcae1844493b
vroyibg <vroyibg@gmail.com>
2018-12-19 08:03:49 +0700

    try set production server on heroku

--- src/Constants.js
+++ src/Constants.js
@@ -2,7 +2,10 @@
  * application constants
  */
 
-export const API_ROOT = "http://localhost:63489/api";
+export const API_ROOT =
+  process.env.NODE_ENV === "development" || process.env.NODE_ENV === "test"
+    ? "http://localhost:63489/api"
+    : "https://lwt-server.herokuapp.com/api";
 export const LOGIN_API = `${API_ROOT}/user/login`;
 export const LOGOUT_API = `${API_ROOT}/user/logout`;
 export const TEXT_API = `${API_ROOT}/text`;
