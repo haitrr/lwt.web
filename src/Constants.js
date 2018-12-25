50b9d1407cf2bfc3f2d528a3103554cf2dbcd954
vroyibg <vroyibg@gmail.com>
2018-12-25 15:40:25 +0700

    switch to gcp vm

--- src/Constants.js
+++ src/Constants.js
@@ -5,7 +5,7 @@
 export const API_ROOT =
   process.env.NODE_ENV === "development" || process.env.NODE_ENV === "test"
     ? "http://localhost:63489/api"
-    : "http://40.83.78.65:5000/api";
+    : "http://35.237.23.105:5000/api";
 export const LOGIN_API = `${API_ROOT}/user/login`;
 export const LOGOUT_API = `${API_ROOT}/user/logout`;
 export const TEXT_API = `${API_ROOT}/text`;
