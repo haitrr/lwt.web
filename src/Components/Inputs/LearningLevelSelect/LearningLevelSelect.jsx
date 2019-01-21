232476a6b81abb5cd46e5582d71dfba6d43395b2
vroyibg <vroyibg@gmail.com>
2019-01-21 23:35:04 +0700

    fix term edit form

--- src/Components/Inputs/LearningLevelSelect/LearningLevelSelect.jsx
+++ src/Components/Inputs/LearningLevelSelect/LearningLevelSelect.jsx
@@ -1,21 +1,24 @@
+import PropTypes from "prop-types";
 import React from "react";
 import { Radio } from "antd";
 
-class LearningLevelSelect extends React.Component {
-  render() {
-    const { value, onChange } = this.props;
-    return (
-      <Radio.Group onChange={onChange} value={value} buttonStyle="solid">
-        <Radio.Button value={2}> Learning 1</Radio.Button>
-        <Radio.Button value={3}> Learning 2</Radio.Button>
-        <Radio.Button value={4}> Learning 3</Radio.Button>
-        <Radio.Button value={5}> Learning 4</Radio.Button>
-        <Radio.Button value={6}> Learning 5</Radio.Button>
-        <Radio.Button value={7}> Well Known</Radio.Button>
-        <Radio.Button value={0}> Ignore</Radio.Button>
-      </Radio.Group>
-    );
-  }
+export default function LearningLevelSelect(props) {
+  const { value, onChange } = props;
+  return (
+    <Radio.Group onChange={onChange} value={value}>
+      <Radio.Button value={1}>Unknown</Radio.Button>
+      <Radio.Button value={2}>Learning 1</Radio.Button>
+      <Radio.Button value={3}>Learning 2</Radio.Button>
+      <Radio.Button value={4}>Learning 3</Radio.Button>
+      <Radio.Button value={5}>Learning 4</Radio.Button>
+      <Radio.Button value={6}>Learning 5</Radio.Button>
+      <Radio.Button value={7}>Well Known</Radio.Button>
+      <Radio.Button value={0}>Ignore</Radio.Button>
+    </Radio.Group>
+  );
 }
 
-export default LearningLevelSelect;
+LearningLevelSelect.propTypes = {
+  onChange: PropTypes.func.isRequired,
+  value: PropTypes.number.isRequired
+};
