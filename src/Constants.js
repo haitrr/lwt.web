13e3ac59f7f10659170adc30745c873dc31ff3f6
vroyibg <vroyibg@gmail.com>
2020-02-23 07:52:13 +0700

    remove old api

--- src/Constants.js
+++ src/Constants.js
@@ -2,8 +2,8 @@
  * application constants
  */
 
-export const API_ROOT = "http://lwtapi.hai2cs.com/api";
-export const DICTIONARY_API_ROOT = "http://dictionary.hai2cs.com";
+export const API_ROOT = "API_ROOT";
+export const DICTIONARY_API_ROOT = "DICTIONARY_API_ROOT";
 export const LOGIN_API = `${API_ROOT}/user/login`;
 export const LOGOUT_API = `${API_ROOT}/user/logout`;
 export const TEXT_API = `${API_ROOT}/text`;
