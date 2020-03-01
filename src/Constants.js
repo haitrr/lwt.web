aeb27f012578b5ac5b4f1ccacbfada46484f98c3
vroyibg <vroyibg@gmail.com>
2020-03-01 15:17:28 +0700

    use .env

--- src/Constants.js
+++ src/Constants.js
@@ -2,8 +2,8 @@
  * application constants
  */
 
-export const API_ROOT = "API_ROOT_VALUE";
-export const DICTIONARY_API_ROOT = "DICTIONARY_API_ROOT_VALUE";
+export const API_ROOT = process.env.REACT_APP_API_ROOT;
+export const DICTIONARY_API_ROOT = process.env.REACT_APP_DICTIONARY_API_ROOT;
 export const LOGIN_API = `${API_ROOT}/user/login`;
 export const LOGOUT_API = `${API_ROOT}/user/logout`;
 export const TEXT_API = `${API_ROOT}/text`;
