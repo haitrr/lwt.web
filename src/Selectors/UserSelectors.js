91e9b316e7bc41cc44cd40524216b19b25057745
vroyibg <vroyibg@gmail.com>
2020-03-19 21:53:35 +0700

    fix selector bug

--- src/Selectors/UserSelectors.js
+++ src/Selectors/UserSelectors.js
@@ -1,4 +1,7 @@
 export const selectDictionaryLanguage = (state, language) => {
-  const { code } = state.language.languages.find(l => l.id === language);
+  const code = state.language.languages.find(l => l.id === language)?.code;
+  if (!code) {
+    return null;
+  }
   return state.user.setting.languageSettings[code].dictionaryLanguage;
 };
