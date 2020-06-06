d3e5dfd585ef21b50dfb027f936ccea03f55a0fa
vroyibg <vroyibg@gmail.com>
2020-06-06 23:46:29 +0700

    support parsing query string

---
+++ src/Utilities/queryString.js
@@ -0,0 +1,15 @@
+export const parseQueryString = queryString => {
+  if (!queryString) {
+    return {};
+  }
+  if (queryString[0] !== "?") {
+    throw Error("Invalid query string");
+  }
+  const queries = queryString.substr(1).split("&");
+  const result = {};
+  queries.forEach(q => {
+    const [key, value] = q.split("=");
+    result[key] = value;
+  });
+  return result;
+};
