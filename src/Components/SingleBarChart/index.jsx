c21da83d1fea2a062caef2e21256044851f1aa3a
vroyibg <vroyibg@gmail.com>
2020-08-27 21:20:26 +0700

    better statistic bar

--- src/Components/SingleBarChart/index.jsx
+++ src/Components/SingleBarChart/index.jsx
@@ -2,30 +2,27 @@ import PropTypes from "prop-types";
 import React from "react";
 import styles from "./SingleBarChart.module.scss";
 
-const SingleBarChart = ({ data }) => {
-  const sum = data.map(i => i.value).reduce((a, b) => a + b);
-  return (
-    <span className={styles.bar}>
-      {data.map(item =>
-        item.value !== 0 ? (
-          <span
-            key={item.name}
-            className={styles.section}
-            style={{
-              background: item.color,
-              width: `${(item.value * 100) / sum}%`,
-              textAlign: "center"
-            }}
-          >
-            <span style={{ position: "relative", zIndex: 10 }}>
-              {item.value}
-            </span>
-          </span>
-        ) : null
-      )}
-    </span>
-  );
-};
+const SingleBarChart = ({ data }) => (
+  <span className={styles.bar}>
+    {data.map(item =>
+      item.value !== 0 ? (
+        <span
+          key={item.name}
+          className={styles.section}
+          style={{
+            background: item.color,
+            paddingLeft: "5px",
+            paddingRight: "5px",
+            flex: item.value,
+            textAlign: "center"
+          }}
+        >
+          {item.value}
+        </span>
+      ) : null
+    )}
+  </span>
+);
 
 SingleBarChart.propTypes = {
   data: PropTypes.arrayOf(PropTypes.shape({})).isRequired
