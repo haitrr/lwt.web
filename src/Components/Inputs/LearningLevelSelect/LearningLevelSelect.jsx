9be5a1a32ed9ff24a1f7f900725401c3b2e004c6
vroyibg <vroyibg@gmail.com>
2020-08-28 11:20:19 +0700

    improve term learning level select

--- src/Components/Inputs/LearningLevelSelect/LearningLevelSelect.jsx
+++ src/Components/Inputs/LearningLevelSelect/LearningLevelSelect.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 import React from "react";
 import { Radio } from "antd";
 import styles from "./LearningLevelSelect.module.scss";
-import { TermLearningLevel } from "../../../Enums";
+import { TermLearningLevel, TermLearningColor } from "../../../Enums";
 
 // form item need to be class component.
 // eslint-disable-next-line react/prefer-stateless-function
@@ -16,14 +16,71 @@ export default class LearningLevelSelect extends React.Component {
     const { value, onChange } = this.props;
     return (
       <Radio.Group onChange={onChange} className={styles.select} value={value}>
-        <Radio.Button value={TermLearningLevel.UnKnow}>UK</Radio.Button>
-        <Radio.Button value={TermLearningLevel.Learning1}>L1</Radio.Button>
-        <Radio.Button value={TermLearningLevel.Learning2}>L2</Radio.Button>
-        <Radio.Button value={TermLearningLevel.Learning3}>L3</Radio.Button>
-        <Radio.Button value={TermLearningLevel.Learning4}>L4</Radio.Button>
-        <Radio.Button value={TermLearningLevel.Learning5}>L5</Radio.Button>
-        <Radio.Button value={TermLearningLevel.WellKnow}>WK</Radio.Button>
-        <Radio.Button value={TermLearningLevel.Ignored}>I</Radio.Button>
+        <Radio.Button
+          style={{
+            backgroundColor: TermLearningColor[TermLearningLevel.UnKnow]
+          }}
+          value={TermLearningLevel.UnKnow}
+        >
+          UK
+        </Radio.Button>
+        <Radio.Button
+          style={{
+            backgroundColor: TermLearningColor[TermLearningLevel.Learning1]
+          }}
+          value={TermLearningLevel.Learning1}
+        >
+          L1
+        </Radio.Button>
+        <Radio.Button
+          style={{
+            backgroundColor: TermLearningColor[TermLearningLevel.Learning2]
+          }}
+          value={TermLearningLevel.Learning2}
+        >
+          L2
+        </Radio.Button>
+        <Radio.Button
+          style={{
+            backgroundColor: TermLearningColor[TermLearningLevel.Learning3]
+          }}
+          value={TermLearningLevel.Learning3}
+        >
+          L3
+        </Radio.Button>
+        <Radio.Button
+          style={{
+            backgroundColor: TermLearningColor[TermLearningLevel.Learning4]
+          }}
+          value={TermLearningLevel.Learning4}
+        >
+          L4
+        </Radio.Button>
+        <Radio.Button
+          style={{
+            backgroundColor: TermLearningColor[TermLearningLevel.Learning5]
+          }}
+          value={TermLearningLevel.Learning5}
+        >
+          L5
+        </Radio.Button>
+        <Radio.Button
+          style={{
+            backgroundColor: TermLearningColor[TermLearningLevel.WellKnow]
+          }}
+          value={TermLearningLevel.WellKnow}
+        >
+          WK
+        </Radio.Button>
+        <Radio.Button
+          style={{
+            backgroundColor: TermLearningColor[LearningLevelSelect.UnKnow]
+          }}
+          value={TermLearningLevel.UnKnow}
+          value={TermLearningLevel.Ignored}
+        >
+          I
+        </Radio.Button>
       </Radio.Group>
     );
   }
