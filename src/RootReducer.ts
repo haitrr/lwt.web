2d0ddfbeaa79476649b690993d826de1dae87d3e
vroyibg <vroyibg@gmail.com>
2020-09-27 01:10:07 +0700

    fix things

--- src/RootReducer.ts
+++ src/RootReducer.ts
@@ -4,12 +4,19 @@ import textReducer from "./Reducers/TextReducer";
 import { userReducer } from "./Reducers/UserReducer";
 import termReducer from "./Reducers/TermReducer";
 
+export interface RootState {
+  user: any;
+  language: any;
+  text: any;
+  term: any;
+}
+
 /**
  * root reducer
  */
-export default combineReducers({
+export default combineReducers<RootState>({
   user: userReducer,
   language: languageReducer,
   text: textReducer,
-  term: termReducer
+  term: termReducer,
 });
