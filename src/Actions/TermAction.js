666c1511ed24475f3be849a3b1c8354bfed4011f
vroyibg <vroyibg@gmail.com>
2020-09-27 09:38:51 +0700

    remove unused things

--- src/Actions/TermAction.js
+++ src/Actions/TermAction.js
@@ -3,7 +3,6 @@ import { notification } from "antd";
 import {
   createTermAsync,
   editTermAsync,
-  getTermAsync,
   getTermMeaningAsync,
   getTextMeaningAsync,
 } from "../Apis/TermApi";
@@ -17,27 +16,7 @@ export const TERM_EDITING_MEANING_RESET = "TERM_EDITING_MEANING_RESET";
 export const TERM_GET_MEANING = "TERM_GET_MEANING";
 export const TERM_DICTIONARY = "TERM_DICTIONARY";
 
-export const getTermAction = createAction(TERM_GET, async (id, index) => {
-  try {
-    return { term: await getTermAsync(id), index };
-  } catch (e) {
-    notification.error({ message: "Can't get term", description: e.message });
-    return null;
-  }
-});
-
 export const setEditingTermAction = createAction(TERM_SET, (term) => term);
-export const getEditingTermMeaningAction = createAction(
-  TERM_EDITING_GET_MEANING,
-  async (content, from, to) => {
-    const rs = await getTextMeaningAsync(content.toLowerCase(), from, to);
-    if (!rs) {
-      return "";
-    }
-    return rs.meaning;
-  }
-);
-
 export const dictionaryTermMeaningAction = createAction(
   TERM_DICTIONARY,
   async (content, from, to, index) => {
@@ -87,7 +66,6 @@ export const editTermAction = createAction(TERM_EDITED, async (term) => {
 export const getTermMeaningAction = createAction(
   TERM_GET_MEANING,
   async (term, index) => {
-    console.log(term, index);
     try {
       const termMeaning = await getTermMeaningAsync(term.id);
       return { termMeaning, index };
