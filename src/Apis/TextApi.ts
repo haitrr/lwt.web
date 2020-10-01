acd3e3e32351df485bd32dd46de46a2c8dc4a144
vroyibg <vroyibg@gmail.com>
2020-10-01 19:31:00 +0700

    text api to ts

--- src/Apis/TextApi.ts
+++ src/Apis/TextApi.ts
@@ -7,10 +7,16 @@ import {
   patchAsync,
 } from "../Utilities/HttpRequest";
 
+interface TextFilter {}
+
 /**
  * Get the list of text
  */
-export async function getTextsAsync(filters, page, itemPerPage) {
+export async function getTextsAsync(
+  filters: TextFilter,
+  page: number,
+  itemPerPage: number
+) {
   try {
     return await getAsync(TEXT_API, {
       ...filters,
@@ -22,11 +28,13 @@ export async function getTextsAsync(filters, page, itemPerPage) {
   }
 }
 
+interface TextCreateModel {}
+
 /**
  * create a text
  * @param text the text to create
  */
-export async function createTextAsync(text) {
+export async function createTextAsync(text: TextCreateModel) {
   try {
     await postAsync(TEXT_API, text);
 
@@ -40,7 +48,7 @@ export async function createTextAsync(text) {
  * create a text
  * @param textId the text id to get
  */
-export async function getTextReadAsync(textId) {
+export async function getTextReadAsync(textId: number) {
   try {
     return await getAsync(`${TEXT_API}/${textId}`, null);
   } catch (e) {
@@ -48,37 +56,43 @@ export async function getTextReadAsync(textId) {
   }
 }
 
-export async function getTermCountByLearningLevelAsync(textId) {
+export async function getTermCountByLearningLevelAsync(textId: number) {
   return getAsync(`${TEXT_API}/${textId}/term-counts`, null);
 }
 
-export async function deleteTextAsync(textId) {
+export async function deleteTextAsync(textId: number) {
   return deleteAsync(`${TEXT_API}/${textId}`);
 }
 
-export async function editTextAsync(id, text) {
+interface TextEditModel {}
+
+export async function editTextAsync(id: number, text: TextEditModel) {
   return putAsync(TEXT_API, id, text);
 }
 
-export async function getTextEditDetailAsync(textId) {
+export async function getTextEditDetailAsync(textId: number) {
   return getAsync(`${TEXT_API}/${textId}/edit-details`);
 }
 
-export async function setTextBookmarkAsync(id, index) {
+export async function setTextBookmarkAsync(id: number, index: number) {
   return patchAsync(`${TEXT_API}`, id, "bookmark", { termIndex: index });
 }
-export async function getTextTermsAsync(textId, indexFrom, indexTo) {
+export async function getTextTermsAsync(
+  textId: number,
+  indexFrom: number,
+  indexTo: number
+) {
   return getAsync(`${TEXT_API}/${textId}/terms`, { indexFrom, indexTo });
 }
 
-export async function getTermCountInTextAsync(termId, textId) {
+export async function getTermCountInTextAsync(termId: number, textId: number) {
   return getAsync(`${TEXT_API}/${textId}/terms/${termId}/count`);
 }
 
-export async function getTermCountAsync(textId) {
+export async function getTermCountAsync(textId: number) {
   return getAsync(`${TEXT_API}/${textId}/term-count`);
 }
 
-export async function getProcessedIndexAsync(textId) {
+export async function getProcessedIndexAsync(textId: number) {
   return getAsync(`${TEXT_API}/${textId}/processed-index`);
 }
