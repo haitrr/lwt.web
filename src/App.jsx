0584e6bb18409de70cf2db756e3caa716af5ea0b
vroyibg <vroyibg@gmail.com>
2020-11-10 00:38:15 +0700

    fix mobile sticky

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,20 @@ import { getSettingAction } from "./Actions/UserAction";
 import UserPage from "./Components/Pages/UserPage";
 import Themer from "./Themer";
 
+const calculateVh = () => {
+  // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+  const vh = window.innerHeight * 0.01;
+  // Then we set the value in the --vh custom property to the root of the document
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+};
+
+calculateVh();
+
+// We listen to the resize event
+window.addEventListener("resize", () => {
+  calculateVh();
+});
+
 /**
  * app.
  */
