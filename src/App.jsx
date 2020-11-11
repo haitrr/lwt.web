8197e5e49cdd214a30f609a38560281dee3deac8
vroyibg <vroyibg@gmail.com>
2020-11-11 20:19:21 +0700

    fix layout

--- src/App.jsx
+++ src/App.jsx
@@ -42,7 +42,7 @@ class App extends React.Component {
   render() {
     return (
       <Themer>
-        <Paper style={{ height: "100%" }}>
+        <Paper style={{ minHeight: "-webkit-fill-available" }}>
           <BrowserRouter>
             <div className={styles.layout}>
               <Helmet>
