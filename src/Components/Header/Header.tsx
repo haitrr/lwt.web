fdd42499370239b5bda27a860971ccb64b862a9a
vroyibg <vroyibg@gmail.com>
2020-11-12 09:22:23 +0700

    fix header

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,23 +1,27 @@
 import React from "react";
-import { AppBar, Button, Toolbar } from "@material-ui/core";
+import { AppBar, Button, IconButton, Toolbar } from "@material-ui/core";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import MenuBookIcon from "@material-ui/icons/MenuBook";
+import { useHistory } from "react-router";
+import HomeIcon from "@material-ui/icons/Home";
 import styles from "./Header.module.scss";
 import UserMenu from "./UserMenu";
 import { RootState } from "../../RootReducer";
 
 const Header = () => {
   const isLoggedIn = useSelector((state: RootState) => state.user.isLoggedIn);
+  const history = useHistory();
   return (
     <AppBar position="sticky">
       <Toolbar style={{ display: "flex", justifyContent: "space-between" }}>
         <div style={{ display: "flex", flexDirection: "row" }}>
-          <Link className={styles.navigationLink} to="/">
-            <Button>Home</Button>
-          </Link>
-          <Link className={styles.navigationLink} to="/text">
-            <Button>Text</Button>
-          </Link>
+          <IconButton onClick={() => history.push("/")}>
+            <HomeIcon fontSize="large" color="secondary" />
+          </IconButton>
+          <IconButton onClick={() => history.push("/text")}>
+            <MenuBookIcon fontSize="large" color="secondary" />
+          </IconButton>
         </div>
         <div>
           {isLoggedIn ? (
