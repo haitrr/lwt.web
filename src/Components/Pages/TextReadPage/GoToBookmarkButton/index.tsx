71c5706ad8450f32f5f0d4062e5ac07f08c33a48
vroyibg <vroyibg@gmail.com>
2020-11-21 01:10:40 +0700

    show percentage in go to bookmark button

---
+++ src/Components/Pages/TextReadPage/GoToBookmarkButton/index.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Fab } from "@material-ui/core";
+import { connect } from "react-redux";
+import styles from "./GoToBookmarkButton.module.scss";
+import { RootState } from "../../../../RootReducer";
+
+interface OwnProps {
+  onClick: () => void;
+}
+
+interface StateProps {
+  current: number;
+  total: number;
+}
+
+type Props = OwnProps & StateProps;
+
+const GoToBookmarkButton: React.FC<Props> = ({ onClick, current, total }) => (
+  <Fab
+    title="Go to bookmark"
+    type="button"
+    color="primary"
+    className={styles.button}
+    onClick={onClick}
+  >
+    {`${Math.round((current / total) * 100 * 100) / 100}%`}
+  </Fab>
+);
+
+export default connect<StateProps, null, OwnProps, RootState>(
+  (state: RootState) => {
+    if (!state.text.readingText) throw new Error();
+    return {
+      current: state.text.readingText.bookmark,
+      total: state.text.readingText.termCount,
+    };
+  }
+)(GoToBookmarkButton);
