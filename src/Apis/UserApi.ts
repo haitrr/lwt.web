46b8f1b31a85c16bb2376277055050773165cf2f
Hai Tran <vroyibg@gmail.com>
2021-11-11 18:42:40 +0700

    ts

--- src/Apis/UserApi.ts
+++ src/Apis/UserApi.ts
@@ -6,15 +6,23 @@ export function getSettingAsync() {
   return getAsync(`${API_ROOT}/user/setting`);
 }
 
-export function updateSettingAsync(settings) {
+export interface Setting {
+
+}
+
+export function updateSettingAsync(settings: Setting) {
   return putAsync(`${API_ROOT}/user`, "setting", settings);
+}
+
+export interface Credentials {
+
 }
 
 /**
  * login a user async
  * @param data login data
  */
-export async function loginAsync(data) {
+export async function loginAsync(data: Credentials) {
   try {
     const result = await postAsync(`${API_ROOT}/user/login`, data);
     const { token } = result;
@@ -30,6 +38,10 @@ export function logout() {
   localStorage.removeItem(TOKEN_LOCAL_STORAGE_KEY);
 }
 
-export async function registerAsync(data) {
+export interface RegisterData {
+
+}
+
+export async function registerAsync(data: RegisterData) {
   await postAsync(`${API_ROOT}/user`, data);
 }
