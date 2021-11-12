6a82828e174f11966e8a530e219bf24312138d34
Hai Tran <vroyibg@gmail.com>
2021-11-12 12:37:14 +0700

    login form to fc

--- src/Components/Forms/LoginForm/LoginForm.tsx
+++ src/Components/Forms/LoginForm/LoginForm.tsx
@@ -1,10 +1,11 @@
-import { Form } from "antd";
-import { Button, TextField } from "@material-ui/core";
-import React from "react";
-import { connect } from "react-redux";
-import { loginAction } from "../../../Actions/UserAction";
+import {Form} from "antd";
+import {Button, TextField} from "@material-ui/core";
+import React, {useState} from "react";
+import {connect} from "react-redux";
+import {loginAction} from "../../../Actions/UserAction";
 
-interface StateProps {}
+interface StateProps {
+}
 
 interface DispatchProps {
   login: Function;
@@ -21,59 +22,43 @@ interface FormValues {
   password: string;
 }
 
-interface State {
-  loading: boolean;
-}
-
-/**
- * login form.
- */
-class LoginForm extends React.Component<Props, State> {
-  state = { loading: false };
-
-  handleLogin = (data: FormValues) => {
-    const { login } = this.props;
-    this.setState({ loading: true }, () =>
-      login(data).then(() => this.setState({ loading: false }))
-    );
+const LoginForm: React.FC<Props> = ({className, login}) => {
+  const [loading, setLoading] = useState(false)
+  const handleLogin = (data: FormValues) => {
+    setLoading(true)
+    login(data).then(() => setLoading(false))
   };
-
-  render() {
-    const { className } = this.props;
-    const { loading } = this.state;
-
-    return (
-      <Form className={className} onFinish={this.handleLogin}>
-        <h1>LOGIN</h1>
-        <Form.Item name="userName" rules={[{ required: true }]}>
-          <TextField
-            variant="outlined"
-            style={{ width: "100%", marginBottom: "1rem" }}
-            placeholder="User name"
-          />
-        </Form.Item>
-        <Form.Item name="password" rules={[{ required: true }]}>
-          <TextField
-            variant="outlined"
-            style={{ width: "100%" }}
-            type="password"
-            placeholder="Password"
-          />
-        </Form.Item>
-        <Form.Item>
-          <Button
-            disabled={loading}
-            style={{ marginTop: "1rem" }}
-            variant="contained"
-            color="primary"
-            type="submit"
-          >
-            Login
-          </Button>
-        </Form.Item>
-      </Form>
-    );
-  }
+  return (
+    <Form className={className} onFinish={handleLogin}>
+      <h1>LOGIN</h1>
+      <Form.Item name="userName" rules={[{required: true}]}>
+        <TextField
+          variant="outlined"
+          style={{width: "100%", marginBottom: "1rem"}}
+          placeholder="User name"
+        />
+      </Form.Item>
+      <Form.Item name="password" rules={[{required: true}]}>
+        <TextField
+          variant="outlined"
+          style={{width: "100%"}}
+          type="password"
+          placeholder="Password"
+        />
+      </Form.Item>
+      <Form.Item>
+        <Button
+          disabled={loading}
+          style={{marginTop: "1rem"}}
+          variant="contained"
+          color="primary"
+          type="submit"
+        >
+          Login
+        </Button>
+      </Form.Item>
+    </Form>
+  );
 }
 
-export default connect(null, { login: loginAction })(LoginForm);
+export default connect(null, {login: loginAction})(LoginForm);
