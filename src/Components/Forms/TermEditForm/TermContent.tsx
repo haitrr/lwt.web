af50f3d3803f61eb99cccbda0b9ef3d7ed151649
Hai Tran <vroyibg@gmail.com>
2021-11-12 15:13:21 +0700

    migrate to ts

--- src/Components/Forms/TermEditForm/TermContent.tsx
+++ src/Components/Forms/TermEditForm/TermContent.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { toast } from "react-toastify";
 import { CopyToClipboard } from "react-copy-to-clipboard";
-import PropTypes from "prop-types";
 import styles from "./TermEditForm.module.scss";
 import { importantColors } from "../../../Enums";
+import {Term} from "../../../Reducers/TextReducer";
 
-const TermContent = ({ term }) => (
+interface Props {
+  term: Term;
+}
+
+const TermContent: React.FC<Props> = ({ term }) => (
   <CopyToClipboard
     text={term.content}
     onCopy={() => {
@@ -29,9 +33,5 @@ const TermContent = ({ term }) => (
     </div>
   </CopyToClipboard>
 );
-
-TermContent.propTypes = {
-  term: PropTypes.shape({}).isRequired
-};
 
 export default TermContent;
