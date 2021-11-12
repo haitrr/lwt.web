378586f5414b3049a887c22966d5d6711fdd7669
Hai Tran <vroyibg@gmail.com>
2021-11-12 15:21:34 +0700

    migrate to fc

--- src/Components/Forms/RegisterForm/RegisterForm.tsx
+++ src/Components/Forms/RegisterForm/RegisterForm.tsx
@@ -1,12 +1,13 @@
-import { Form } from "antd";
-import { toast } from "react-toastify";
-import { Button, TextField } from "@material-ui/core";
+import {Form} from "antd";
+import {toast} from "react-toastify";
+import {Button, TextField} from "@material-ui/core";
 import React from "react";
-import { RouteComponentProps, withRouter } from "react-router";
-import { connect } from "react-redux";
-import { registerAction } from "../../../Actions/UserAction";
+import {RouteComponentProps, useHistory, withRouter} from "react-router";
+import {connect} from "react-redux";
+import {registerAction} from "../../../Actions/UserAction";
 
-interface OwnProps { }
+interface OwnProps {
+}
 
 interface DispatchProps {
   register: Function;
@@ -18,87 +19,72 @@ interface FormValues {
   repeatPassword: string;
 }
 
-interface State {
-  submitting: boolean;
-}
-
 type Props = OwnProps & DispatchProps & RouteComponentProps;
 
-/**
- * Register form
- */
-class RegisterForm extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    this.state = { submitting: false };
-  }
+const RegisterForm: React.FC<Props> = ({register}) => {
+  const [submitting, setSubmitting] = React.useState(false)
 
-  handleRegister = (data: any): Promise<any> => {
-    const { register, history } = this.props;
+  const handleRegister = (data: any): Promise<any> => {
+    const history = useHistory()
     return register(data).then(() => {
       history.push("/login");
     });
   };
 
-  handleSubmit = (values: FormValues) => {
-    const { history } = this.props;
+  const handleSubmit = (values: FormValues) => {
+    const history = useHistory()
     if (values.password !== values.repeatPassword) {
       toast.error("Passwords not match");
       return;
     }
 
-    this.setState({ submitting: true }, () =>
-      this.handleRegister(values)
-        .then(() => {
-          history.push("/login");
-        })
-        .catch((e: Error) => {
-          this.setState({ submitting: false });
-        })
-    );
+    setSubmitting(true)
+    handleRegister(values)
+      .then(() => {
+        history.push("/login");
+      })
+      .catch((e: Error) => {
+        setSubmitting(false)
+      })
   };
 
-  render() {
-    const { submitting } = this.state;
-
-    return (
-      <Form onFinish={this.handleSubmit}>
-        <Form.Item name="userName">
-          <TextField
-            variant="outlined"
-            style={{ width: "100%", marginBottom: "1rem" }}
-            placeholder="User Name"
-          />
-        </Form.Item>
-        <Form.Item name="password">
-          <TextField
-            variant="outlined"
-            style={{ width: "100%", marginBottom: "1rem" }}
-            type="password"
-            placeholder="Password"
-          />
-        </Form.Item>
-        <Form.Item name="repeatPassword">
-          <TextField
-            variant="outlined"
-            style={{ width: "100%", marginBottom: "1rem" }}
-            type="password"
-            placeholder="Retype password"
-          />
-        </Form.Item>
-        <Button
-          disabled={submitting}
-          variant="contained"
-          color="primary"
-          type="submit"
-        >
-          Register
-        </Button>
-      </Form>
-    );
-  }
+  return (
+    <Form onFinish={handleSubmit}>
+      <Form.Item name="userName">
+        <TextField
+          variant="outlined"
+          style={{width: "100%", marginBottom: "1rem"}}
+          placeholder="User Name"
+        />
+      </Form.Item>
+      <Form.Item name="password">
+        <TextField
+          variant="outlined"
+          style={{width: "100%", marginBottom: "1rem"}}
+          type="password"
+          placeholder="Password"
+        />
+      </Form.Item>
+      <Form.Item name="repeatPassword">
+        <TextField
+          variant="outlined"
+          style={{width: "100%", marginBottom: "1rem"}}
+          type="password"
+          placeholder="Retype password"
+        />
+      </Form.Item>
+      <Button
+        disabled={submitting}
+        variant="contained"
+        color="primary"
+        type="submit"
+      >
+        Register
+      </Button>
+    </Form>
+  );
 }
 
 export default withRouter(
-  connect(null, { register: registerAction })(RegisterForm)
+  connect(null, {register: registerAction})(RegisterForm)
 );
