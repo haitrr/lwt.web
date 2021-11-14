ac941e1da89e64cc7b5f327b4fe59bd1670839bc
Hai Tran <vroyibg@gmail.com>
2021-11-14 12:30:24 +0700

    term edit page fc

--- src/Components/Pages/TermEditPage/TermEditPage.jsx
+++ src/Components/Pages/TermEditPage/TermEditPage.jsx
@@ -3,18 +3,16 @@ import React from "react";
 /**
  * term edit page.
  */
-class TermEditPage extends React.Component {
-  render() {
-    const { term } = this.props;
+const TermEditPage = props => {
+  const {term} = props;
 
-    return (
-      <div>
-        <p>{term.content}</p>
-        <p>{term.meaning}</p>
-        <p>{term.learningLevel}</p>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <p>{term.content}</p>
+      <p>{term.meaning}</p>
+      <p>{term.learningLevel}</p>
+    </div>
+  );
+};
 
 export { TermEditPage };
