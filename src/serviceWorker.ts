736da4eb60416076b87676656d92732c8f7f3214
Hai Tran <vroyibg@gmail.com>
2021-11-20 15:00:59 +0700

    fix ts

--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -19,7 +19,7 @@ const isLocalhost = Boolean(
 export function register(config: any) {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
-    const publicUrl = new URL(process.env.PUBLIC_URL!, window.location.toString())
+    const publicUrl = new URL(process.env.PUBLIC_URL, window.location.toString())
     if (publicUrl.origin !== window.location.origin) {
       // Our service worker won't work if PUBLIC_URL is on a different origin
       // from what our page is served on. This might happen if a CDN is used to
@@ -93,7 +93,7 @@ function checkValidServiceWorker(swUrl: string, config: any) {
   fetch(swUrl)
     .then(response => {
       // Ensure service worker exists, and that we really are getting a JS file.
-      if (response.status === 404 || response.headers.get('content-type')!.indexOf('javascript') === -1) {
+      if (response.status === 404 || response.headers.get('content-type')?.indexOf('javascript') === -1) {
         // No service worker found. Probably a different app. Reload the page.
         navigator.serviceWorker.ready.then(registration => {
           registration.unregister().then(() => {
