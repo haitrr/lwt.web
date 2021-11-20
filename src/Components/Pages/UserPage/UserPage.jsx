1e22381164f5c4d910bd74faf4252584057ff08f
Hai Tran <vroyibg@gmail.com>
2021-11-20 07:21:56 +0700

    user page fc

--- src/Components/Pages/UserPage/UserPage.jsx
+++ src/Components/Pages/UserPage/UserPage.jsx
@@ -1,23 +1,16 @@
 import PropTypes from "prop-types";
 import React from "react";
-import { connect } from "react-redux";
-import { Field, Form, withFormik } from "formik";
-import { Button } from "@material-ui/core";
+import {connect} from "react-redux";
+import {Field, Form, withFormik} from "formik";
+import {Button} from "@material-ui/core";
 import styles from "./UserPage.module.scss";
-import { updateSettingAction } from "../../../Actions/UserAction";
-import { LanguageCode } from "../../../Enums";
+import {updateSettingAction} from "../../../Actions/UserAction";
+import {LanguageCode} from "../../../Enums";
 import LanguageSelect from "../../Inputs/LanguageSelect/LanguageSelect";
 
-class UserPage extends React.Component {
-  state = { currentLanguage: "en" };
-
-  onChangeLanguage = (event) => {
-    this.setState({ currentLanguage: event.target.value });
-  };
-
-  onAddLanguageSetting = () => {
-    const { languages, setFieldValue } = this.props;
-    const { languageSettings } = this.props.values;
+const UserPage = ({languages, setFieldValue, values, handleChange}) => {
+  const onAddLanguageSetting = () => {
+    const {languageSettings} = values;
     const newLanguageSettings = [...languageSettings];
     for (let i = 0; i < languages.length; i += 1) {
       const l = languages[i];
@@ -32,49 +25,46 @@ class UserPage extends React.Component {
     setFieldValue("languageSettings", newLanguageSettings);
   };
 
-  render() {
-    const { values, languages, handleChange } = this.props;
-    if (!languages || !values.languageSettings) {
-      return <h1>Loading</h1>;
-    }
-    return (
-      <div className={styles.root}>
-        <Form>
-          <h1>Dictionary settings</h1>
-          <br />
-          {values.languageSettings
-            ? values.languageSettings.map((ls, i) => (
-                <div>
-                  <h1>
-                    {languages.find((l) => l.code === ls.languageCode).name}
-                  </h1>
-                  <Field
-                    component={LanguageSelect}
-                    name={`languageSettings[${i}].dictionaryLanguageCode`}
-                    onChange={handleChange}
-                    value={ls.dictionaryLanguageCode}
-                  />
-                </div>
-              ))
-            : null}
-          <br />
-          <Button
-            variant="contained"
-            color="secondary"
-            disabled={languages.length <= values.languageSettings.length}
-            type="button"
-            onClick={this.onAddLanguageSetting}
-          >
-            Add
-          </Button>
-          <br />
-          <Button variant="contained" color="primary" type="submit">
-            Save
-          </Button>
-        </Form>
-      </div>
-    );
+  if (!languages || !values.languageSettings) {
+    return <h1>Loading</h1>;
   }
+  return (
+    <div className={styles.root}>
+      <Form>
+        <h1>Dictionary settings</h1>
+        <br/>
+        {values.languageSettings
+          ? values.languageSettings.map((ls, i) => (
+            <div>
+              <h1>
+                {languages.find((l) => l.code === ls.languageCode).name}
+              </h1>
+              <Field
+                component={LanguageSelect}
+                name={`languageSettings[${i}].dictionaryLanguageCode`}
+                onChange={handleChange}
+                value={ls.dictionaryLanguageCode}
+              />
+            </div>
+          ))
+          : null}
+        <br/>
+        <Button
+          variant="contained"
+          color="secondary"
+          disabled={languages.length <= values.languageSettings.length}
+          type="button"
+          onClick={onAddLanguageSetting}
+        >
+          Add
+        </Button>
+        <br/>
+        <Button variant="contained" color="primary" type="submit">
+          Save
+        </Button>
+      </Form>
+    </div>
+  );
 }
 
 UserPage.propTypes = {
@@ -93,7 +83,7 @@ const connectedUserPage = connect(
   }
 )(
   withFormik({
-    handleSubmit: (values, { props }) => {
+    handleSubmit: (values, {props}) => {
       props.updateUserSetting(values);
     },
     enableReinitialize: true,
