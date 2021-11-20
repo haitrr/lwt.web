5fced3224004021855c1b3a14edc4a13dde37b7d
Hai Tran <vroyibg@gmail.com>
2021-11-20 07:26:01 +0700

    term button fc

--- src/Components/Term/TermButton.tsx
+++ src/Components/Term/TermButton.tsx
@@ -1,17 +1,18 @@
 import React from "react";
-import { connect } from "react-redux";
+import {connect} from "react-redux";
 import classNames from "classnames";
 import styles from "./Term.module.scss";
-import { setBookmarkAction, selectTermAction } from "../../Actions/TextAction";
+import {setBookmarkAction, selectTermAction} from "../../Actions/TextAction";
 import {
   importantColors,
   isLearningTerm,
   TermLearningColor,
 } from "../../Enums";
-import { RootState } from "../../RootReducer";
-import { Term } from "../../Reducers/TextReducer";
+import {RootState} from "../../RootReducer";
+import {Term} from "../../Reducers/TextReducer";
 
 export const LAST_BEGIN_INDEX_ID = "last-begin-index";
+
 interface StateProps {
   id: number;
   isLastBeginIndex: boolean;
@@ -30,61 +31,52 @@ interface DispatchProps {
 
 type Props = StateProps & OwnProps & DispatchProps;
 
-class TermButton extends React.Component<Props> {
-  buttonRef: React.RefObject<HTMLSpanElement>;
+const TermButton: React.FC<Props> = ({setBookmark, id, bookmark, isLastBeginIndex, selectTerm, term, onClick}) => {
+  const buttonRef = React.useRef<HTMLSpanElement>(null);
 
-  constructor(props: Props) {
-    super(props);
-    this.buttonRef = React.createRef();
-  }
-
-  onTermClick = (e: React.MouseEvent | React.KeyboardEvent) => {
-    const { setBookmark, id, selectTerm, term, onClick } = this.props;
+  const onTermClick = (e: React.MouseEvent | React.KeyboardEvent) => {
     selectTerm(term.index);
     setBookmark(id, term.index);
     onClick(e);
   };
 
-  render() {
-    const { term, bookmark, isLastBeginIndex } = this.props;
-    const className = classNames(
-      styles.term,
-      TermLearningColor[term.learningLevel],
-      {
-        [styles.bookmark]: bookmark,
-        [styles.termCount]: term.count && isLearningTerm(term.learningLevel),
-      }
-    );
-
-    let id;
-    if (bookmark) {
-      id = "bookmark";
-    } else if (isLastBeginIndex) {
-      id = LAST_BEGIN_INDEX_ID;
+  const className = classNames(
+    styles.term,
+    TermLearningColor[term.learningLevel],
+    {
+      [styles.bookmark]: bookmark,
+      [styles.termCount]: term.count && isLearningTerm(term.learningLevel),
     }
+  );
 
-    const s =
-      term.count && isLearningTerm(term.learningLevel)
-        ? {
-            borderBottomColor: importantColors[Math.min(term.count, 49)],
-          }
-        : undefined;
+  let termId;
+  if (bookmark) {
+    termId = "bookmark";
+  } else if (isLastBeginIndex) {
+    termId = LAST_BEGIN_INDEX_ID;
+  }
 
-    return (
-      <span
-        tabIndex={-1}
-        role="button"
-        id={id}
-        className={className}
-        style={s}
-        ref={this.buttonRef}
-        onClick={this.onTermClick}
-        onKeyDown={this.onTermClick}
-      >
+  const s =
+    term.count && isLearningTerm(term.learningLevel)
+      ? {
+        borderBottomColor: importantColors[Math.min(term.count, 49)],
+      }
+      : undefined;
+
+  return (
+    <span
+      tabIndex={-1}
+      role="button"
+      id={termId}
+      className={className}
+      style={s}
+      ref={buttonRef}
+      onClick={onTermClick}
+      onKeyDown={onTermClick}
+    >
         {term.content}
       </span>
-    );
-  }
+  );
 }
 
 export default connect(
