c3f859334604d775fe39a20226143d6df52eb3ff
Hai Tran <vroyibg@gmail.com>
2021-11-20 13:04:16 +0700

    query string ts

--- src/Utilities/queryString.ts
+++ src/Utilities/queryString.ts
@@ -1,4 +1,4 @@
-export const parseQueryString = queryString => {
+export const parseQueryString = (queryString: string) => {
   if (!queryString) {
     return {};
   }
@@ -6,7 +6,7 @@ export const parseQueryString = queryString => {
     throw Error("Invalid query string");
   }
   const queries = queryString.substr(1).split("&");
-  const result = {};
+  const result: { [key: string]: string } = {};
   queries.forEach(q => {
     const [key, value] = q.split("=");
     result[key] = value;
