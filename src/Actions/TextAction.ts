80af9d9bae60f8571702650c14597f6b1a1f8ff7
Hai Tran <vroyibg@gmail.com>
2021-12-25 12:09:57 +0700

    remove get processed term action

--- src/Actions/TextAction.ts
+++ src/Actions/TextAction.ts
@@ -1,150 +1,116 @@
 import { TextCreateModel } from './../Apis/TextApi';
-import { toast } from "react-toastify";
-import { createAction } from "redux-actions";
+import { toast } from 'react-toastify';
+import { createAction } from 'redux-actions';
 import {
-  createTextAsync,
-  deleteTextAsync,
-  editTextAsync,
-  getTermCountByLearningLevelAsync,
-  getTextEditDetailAsync,
-  getTextReadAsync,
-  getTextTermsAsync,
-  setTextBookmarkAsync,
-  getTermCountInTextAsync,
-  getTermCountAsync,
-  getProcessedTermCountAsync,
-} from "../Apis/TextApi";
+    createTextAsync,
+    deleteTextAsync,
+    editTextAsync,
+    getTermCountByLearningLevelAsync,
+    getTextEditDetailAsync,
+    getTextReadAsync,
+    getTextTermsAsync,
+    setTextBookmarkAsync,
+    getTermCountInTextAsync,
+    getTermCountAsync,
+} from '../Apis/TextApi';
 
-export const TEXT_CREATED = "TEXT_CREATED";
-export const TEXT_DELETED = "TEXT_DELETED";
-export const TEXT_READ = "TEXT_READ";
-export const TEXT_EDITED = "TEXT_EDITED";
-export const TEXT_EDIT_DETAIL_FETCHED = "TEXT_EDIT_DETAIL_FETCHED";
-export const TEXT_BOOKMARK_SET = "TEXT_BOOKMARK_SET";
-export const TEXT_TERM_SELECT = "TEXT_TERM_SELECT";
-export const TERM_COUNT_LOADED = "TERM_COUNT_LOADED";
-export const READING_TEXT_TERMS_COUNT_LOADED =
-  "READING_TEXT_TERMS_COUNT_LOADED";
-export const TEXT_TERM_LOADED = "TEXT_TERM_LOADED";
-export const TERM_INDEX_BEGIN_SET = "TERM_INDEX_BEGIN_SET";
-export const TERM_INDEX_END_SET = "TERM_INDEX_END_SET";
-export const TERM_COUNT_IN_TEXT = "TERM_COUNT_IN_TEXT";
-export const TEXT_TERM_COUNT_GET = "TEXT_TERM_COUNT_GET";
-export const TEXT_PROCESSED_TERM_COUNT_GET = "TEXT_PROCESSED_TERM_COUNT_GET";
-export const VIEWING_TERM_SET = "VIEWING_TERM_SET";
+export const TEXT_CREATED = 'TEXT_CREATED';
+export const TEXT_DELETED = 'TEXT_DELETED';
+export const TEXT_READ = 'TEXT_READ';
+export const TEXT_EDITED = 'TEXT_EDITED';
+export const TEXT_EDIT_DETAIL_FETCHED = 'TEXT_EDIT_DETAIL_FETCHED';
+export const TEXT_BOOKMARK_SET = 'TEXT_BOOKMARK_SET';
+export const TEXT_TERM_SELECT = 'TEXT_TERM_SELECT';
+export const TERM_COUNT_LOADED = 'TERM_COUNT_LOADED';
+export const READING_TEXT_TERMS_COUNT_LOADED = 'READING_TEXT_TERMS_COUNT_LOADED';
+export const TEXT_TERM_LOADED = 'TEXT_TERM_LOADED';
+export const TERM_INDEX_BEGIN_SET = 'TERM_INDEX_BEGIN_SET';
+export const TERM_INDEX_END_SET = 'TERM_INDEX_END_SET';
+export const TERM_COUNT_IN_TEXT = 'TERM_COUNT_IN_TEXT';
+export const TEXT_TERM_COUNT_GET = 'TEXT_TERM_COUNT_GET';
+export const VIEWING_TERM_SET = 'VIEWING_TERM_SET';
 
 export interface TextFilter {
-  title: string;
-  languageCode: string;
+    title: string;
+    languageCode: string;
 }
 
 /**
  * set reading text.
  */
 
-export const readTextAction = createAction(TEXT_READ, (textId: number) =>
-  getTextReadAsync(textId)
-);
+export const readTextAction = createAction(TEXT_READ, (textId: number) => getTextReadAsync(textId));
 
-export const loadTermCountAction = createAction(
-  TERM_COUNT_LOADED,
-  async (textId: number) => getTermCountByLearningLevelAsync(textId)
+export const loadTermCountAction = createAction(TERM_COUNT_LOADED, async (textId: number) =>
+    getTermCountByLearningLevelAsync(textId),
 );
 
 export const loadReadingTexttermsCountByLearningLevelAction = createAction(
-  READING_TEXT_TERMS_COUNT_LOADED,
-  async (textId: number) => getTermCountByLearningLevelAsync(textId)
+    READING_TEXT_TERMS_COUNT_LOADED,
+    async (textId: number) => getTermCountByLearningLevelAsync(textId),
 );
 
 /**
  * create text action
  */
-export const createTextAction = createAction(TEXT_CREATED, async (text: TextCreateModel) =>
-  createTextAsync(text)
-);
+export const createTextAction = createAction(TEXT_CREATED, async (text: TextCreateModel) => createTextAsync(text));
 
 export const deleteTextAction = createAction(TEXT_DELETED, async (textId: number) => {
-  try {
-    await deleteTextAsync(textId);
-    toast.success("TextItem deleted.");
-    return textId;
-  } catch {
-    toast.error("Can't not delete text, please try again.");
-    return null;
-  }
+    try {
+        await deleteTextAsync(textId);
+        toast.success('TextItem deleted.');
+        return textId;
+    } catch {
+        toast.error("Can't not delete text, please try again.");
+        return null;
+    }
 });
 
 export const editTextAction = createAction(TEXT_EDITED, async (id: number, text: Text) => {
-  try {
-    await editTextAsync(id, text);
-    toast.success("TextItem saved successfully.");
-    return text;
-  } catch {
-    toast.error("Can't not save text , please try again.");
-    return null;
-  }
+    try {
+        await editTextAsync(id, text);
+        toast.success('TextItem saved successfully.');
+        return text;
+    } catch {
+        toast.error("Can't not save text , please try again.");
+        return null;
+    }
 });
 
-export const getTextEditDetailAction = createAction(
-  TEXT_EDIT_DETAIL_FETCHED,
-  async (textId: number) => {
+export const getTextEditDetailAction = createAction(TEXT_EDIT_DETAIL_FETCHED, async (textId: number) => {
     try {
-      return await getTextEditDetailAsync(textId);
+        return await getTextEditDetailAsync(textId);
     } catch {
-      toast.error("Something wen't wrong, please try again.");
-      return null;
+        toast.error("Something wen't wrong, please try again.");
+        return null;
     }
-  }
-);
+});
 
-export const setBookmarkAction = createAction(
-  TEXT_BOOKMARK_SET,
-  async (id: number, index: number) => {
+export const setBookmarkAction = createAction(TEXT_BOOKMARK_SET, async (id: number, index: number) => {
     await setTextBookmarkAsync(id, index);
-  }
-);
-export const selectTermAction = createAction(
-  TEXT_TERM_SELECT,
-  async (index: number) => index
-);
+});
+export const selectTermAction = createAction(TEXT_TERM_SELECT, async (index: number) => index);
 
 export const getTextTermsAction = createAction(
-  TEXT_TERM_LOADED,
-  async (textId: number, indexFrom: number, indexTo: number) => {
-    const result = await getTextTermsAsync(textId, indexFrom, indexTo);
-    return { ...result, begin: indexFrom, end: indexTo };
-  }
+    TEXT_TERM_LOADED,
+    async (textId: number, indexFrom: number, indexTo: number) => {
+        const result = await getTextTermsAsync(textId, indexFrom, indexTo);
+        return { ...result, begin: indexFrom, end: indexTo };
+    },
 );
 
-export const setTermIndexBeginAction = createAction(
-  TERM_INDEX_BEGIN_SET,
-  (begin: number) => begin
-);
-export const setTermIndexEndAction = createAction(
-  TERM_INDEX_END_SET,
-  (end: number) => end
-);
+export const setTermIndexBeginAction = createAction(TERM_INDEX_BEGIN_SET, (begin: number) => begin);
+export const setTermIndexEndAction = createAction(TERM_INDEX_END_SET, (end: number) => end);
 
 export const setViewingTermAction = (index: number) => (dispatch: Function) => {
-  dispatch({ type: VIEWING_TERM_SET, payload: { index } });
+    dispatch({ type: VIEWING_TERM_SET, payload: { index } });
 };
 
-export const getTermCountInTextAction = createAction(
-  TERM_COUNT_IN_TEXT,
-  (termId: number, textId: number) => getTermCountInTextAsync(termId, textId)
+export const getTermCountInTextAction = createAction(TERM_COUNT_IN_TEXT, (termId: number, textId: number) =>
+    getTermCountInTextAsync(termId, textId),
 );
 
-export const getTermCountAction = createAction(
-  TEXT_TERM_COUNT_GET,
-  async (textId: number) => {
+export const getTermCountAction = createAction(TEXT_TERM_COUNT_GET, async (textId: number) => {
     const { termCount } = await getTermCountAsync(textId);
     return { termCount, textId };
-  }
-);
-export const getProcessedTermCountAction = createAction(
-  TEXT_PROCESSED_TERM_COUNT_GET,
-  async (textId: number) => {
-    const { processedTermCount } = await getProcessedTermCountAsync(textId);
-    return { processedTermCount, textId };
-  }
-);
+});
